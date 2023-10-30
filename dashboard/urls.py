728e404d12d6c8cba1672cea16cb940cae480b27
Dinaiym <dinaiym.dubanaeva@alatoo.edu.kg>
2023-10-30 22:59:31 +0600

    youtube search added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path('notes', views.notes, name='notes'),
     path('delete_note/<int:pk>', views.delete_note, name="delete-note"),
     path('notes_detail/<int:pk>', views.NotesDetailView.as_view(), name="notes-details"),
-    path('homework', views.homework, name='"homework'),
-
-
-
+    path('homework', views.homework, name='homework'),
+    path('update_homework/<int:pk>', views.update_homework, name="update-homework"),
+    path('delete_homework/<int:pk>', views.delete_homework, name="delete-homework"),
+    path('youtube', views.youtube, name='youtube'),
     path('events', views.events, name="events"),
+
 ]
